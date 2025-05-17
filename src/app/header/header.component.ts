4bd3b29cc305cc2b99d63b584a3de97187b228f3
sayed <sayed.hossain@exabyting.com>
2025-05-17 19:10:21 +0600

    Add seller section

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,4 +10,21 @@ import { RouterLink } from '@angular/router';
 })
 export class HeaderComponent {
 
+  menuType: String = 'default';
+
+  constructor(private route: Router) { }
+
+  ngOnInit(): void {
+    this.route.events.subscribe((val: any) => {
+      if (val.url) {
+        if(localStorage.getItem('seller') && val.url.includes('seller')) {
+          this.menuType = 'seller';
+        }
+        else{
+          this.menuType = 'default';
+        }
+      }
+   
+    })
+  }
 }
